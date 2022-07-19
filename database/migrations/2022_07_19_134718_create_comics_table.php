eea0ecd898514bec023fff71a76d71b560fe3726
GiuliaV <valente.giulia93@gmail.com>
2022-07-19 15:49:51 +0200

    make:migration create_comics_table

---
+++ database/migrations/2022_07_19_134718_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 200);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 8, 2);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
